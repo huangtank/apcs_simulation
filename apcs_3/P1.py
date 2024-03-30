d59e1bddba197488dfdeff827b2e56b0c29ca1ce
huangtank <1112072@mail.hhjh.hc.edu.tw>
2024-03-30 23:00:29 +0800

    update

--- apcs_3/P1.py
+++ apcs_3/P1.py
@@ -3,20 +3,14 @@ flag = True
 end = ['UAA','UAG','UGA']
 n = 0
 m = 0
-count = 0
 for i in range(len(RNA)-2):
     if RNA[i:i+3] == 'AUG':
         m = i
-        count += 1
         break
 for i in range(m+3, len(RNA)-2, 3):
     if RNA[i:i+3] == 'AUG':
         m = i
-        count += 1
     elif RNA[i:i+3] in end:
         n = i
-        count -= 1
-        if not(count):
-            break
 
 print(len(RNA[m:n])//3)
