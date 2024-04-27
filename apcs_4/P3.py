4dd4dc2f6d18e58160de75aa43bc856ed2674882
huangtank <tank.huang@outlook.com>
2024-04-27 23:52:45 +0800

    update

--- apcs_4/P3.py
+++ apcs_4/P3.py
@@ -12,16 +12,16 @@ for i in arr:
             happy_mon[idx] = 0
     if i == 'W':
         happy_mon[0] += happy[0]
-        happy_time[0] += time[0]
+        happy_time[0] = time[0]
     elif i == 'M':
         happy_mon[1] += happy[1]
-        happy_time[1] += time[1]
+        happy_time[1] = time[1]
     else:
         happy_mon[2] += happy[2]
-        happy_time[2] += time[2]
+        happy_time[2] = time[2]
     if sum(happy_mon) > m:
         ans += 1
     happy_time = [i-1 for i in happy_time]
 print(ans)
 
-# 85分
+# 90分
